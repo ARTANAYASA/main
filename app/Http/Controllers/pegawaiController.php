013de54629efc85f0862c9aeb6c1dc565af0ec0d
ARTANAYASA <artana.mobile@gmail.com>
2022-11-28 13:54:03 +0800

    Menambahkan Tugas templeting dan menggabungkan login dan crud

--- app/Http/Controllers/pegawaiController.php
+++ app/Http/Controllers/pegawaiController.php
@@ -7,110 +7,53 @@ use Illuminate\Http\Request;
 
 class pegawaiController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return view('pegawai.index')->with([
-            'pegawai' => pegawai::all(),
-        ]);
+        $pegawai = pegawai::all();
+        return view('Admin.Pegawai.index')->with('pegawai', $pegawai);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        return view('pegawai.create');
+        return view('Admin.Pegawai.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => 'required|min:3|max:50',
-            'alamat' => 'required|min:3',
-            'email' => 'required|min:3',
+        pegawai::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'alamat' => $request->alamat
         ]);
-
-        $pegawai = new pegawai;
-        $pegawai->name = $request->name;
-        $pegawai->alamat = $request->alamat;
-        $pegawai->email = $request->email;
-        $pegawai->save();
-
-        return to_route('pegawai.index')->with('success','Data Berhasil di Tambah.');
+        return redirect()->route('pegawai.index')->with('success', 'Tambah Data Success');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\pegawai  $pegawai
-     * @return \Illuminate\Http\Response
-     */
-    public function show(pegawai $pegawai)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-       return view('pegawai.edit')->with([
-        'pegawai' => Pegawai::find($id),
-       ]);
+        $pegawai = pegawai::whereId($id)->first();
+        return view('admin.Pegawai.edit')->with('pegawai', $pegawai);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\pegawai  $pegawai
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+
+
+    public function update($id, Request $request)
     {
-        $request->validate([
-            'name' => 'required|min:3|max:50',
-            'alamat' => 'required|min:3',
-            'email' => 'required|min:3',
+        
+
+        pegawai::whereId($id)->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'alamat' => $request->alamat,
+            
         ]);
 
-        $pegawai = Pegawai::find($id);
-        $pegawai->name = $request->name;
-        $pegawai->alamat = $request->alamat;
-        $pegawai->email = $request->email;
-        $pegawai->save();
-
-        return to_route('pegawai.index')->with('success','Data Berhasil di Edit.');
+        return redirect()->route('pegawai.index')->with('success', 'Update Data Success');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\pegawai  $pegawai
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function delete($id)
     {
-        $pegawai = Pegawai::find($id);
-        $pegawai->delete();
-
-        return back()->with('succes','Data Berhasil di Hapus.');
+        pegawai::whereId($id)->delete();
+        return redirect()->back()->with('success', 'Delete Data Success');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\stmikController;
+
 use App\Http\Controllers\pegawaiController;
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,4 +27,27 @@ Route::get('/stmik',[stmikController::class, 'index']);
 
 Route::get('/registrasi',[stmikController::class, 'login']);
 
-Route::resource('pegawai',pegawaiController::class);
+
+Route::get('/login', function () {
+    return view('Auth.Login');
+})->name('login')->middleware('guest');
+
+Route::prefix('auth')->controller(LoginController::class)->group(function () {
+    Route::post('/login', 'login')->name('auth.loginproccess');
+    Route::get('/logout', 'logout')->name('logout');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', [DashboardController::class, 'index']);
+
+
+    Route::get('/pegawai', [pegawaiController::class, 'index'])->name('pegawai.index');
+    Route::get('/create', [pegawaiController::class, 'create'])->name('pegawai.create');
+    Route::post('/createpegawai', [pegawaiController::class, 'store'])->name('pegawai.store');
+
+    Route::get('/edit/{id}', [pegawaiController::class, 'edit'])->name('pegawai.edit');
+
+    Route::post('/update/{id}', [pegawaiController::class, 'update'])->name('pegawai.update');
+
+    Route::get('/delete/{id}', [pegawaiController::class, 'delete'])->name('pegawai.delete');
+});
